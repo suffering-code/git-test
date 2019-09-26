3aa85a0694182539bb6619bbe00ba3b15fb373ae
fyl <478868087@qq.com>
2019-09-26 16:28:29 +0800

    Initial commit

---
+++ src/main/java/com/qf/controller/GitController.java
@@ -0,0 +1,11 @@
+package com.qf.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GitController {
+
+    public static void main(String[] args) {
+        System.out.println("local");
+    }
+}
---
+++ src/main/java/com/qf/spb1905/gittesttt/GitTestttApplication.java
@@ -0,0 +1,13 @@
+package com.qf.spb1905.gittesttt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTestttApplication.class, args);
+    }
+
+}
